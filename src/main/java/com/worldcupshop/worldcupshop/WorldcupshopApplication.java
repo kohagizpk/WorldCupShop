a36dfcb615fe94afe627caab43b91a47f0ce876e
kohagizpk <juliazpk@tutamail.com>
2026-09-11 14:36:29 -0300

    correcao

---
+++ src/main/java/com/worldcupshop/worldcupshop/WorldcupshopApplication.java
@@ -0,0 +1,13 @@
+package com.worldcupshop.worldcupshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldcupshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldcupshopApplication.class, args);
+	}
+
+}
